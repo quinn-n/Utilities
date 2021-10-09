2c1fc7a6b7b5196c48d3db81031b6790ad237cce
quinn_n <quinnneufeld@protonmail.com>
2021-10-08 22:06:10 -0400

    Added support for regular expressions and offloaded CLI args & opts to click

--- search.py
+++ search.py
@@ -7,79 +7,89 @@ Written by Quinn Neufeld. <Quinn Neufeld@gmail.com>
 March 23rd 2019
 Sept. 29 2021 - Removed progutil dependency
 Sept. 29 2021 - Added support for multiple search terms and fuzzy support
+Oct. 8 2021 - Added support for regular expressions and moved arguments and options to click
 """
 from time import sleep
 import os
 from sys import argv
+import re
 
-HELP_MSG = """Usage: search.py <path> <string[s]> [-h] [-v]
--h / --help - prints this message and exits
--v / --verbose - verbose mode
--f / --fuzzy - fuzzy search, passes check if one string matches instead of all strings"""
-VERBOSE = False
-EXCLUDED_SEARCH_TERMS = ("-v", "--verbose", "-h", "--help", "-f", "--fuzzy")
+import click
 
-def verbose_print(s: str):
+VERBOSE = 0
+
+
+def verbose_print(s: str, msg_verbosity: int) -> None:
     """Prints str only if verbose mode is enabled."""
-    if VERBOSE:
-        print(s)
+    if VERBOSE >= msg_verbosity:
+        click.echo(s)
 
-def search_file(path: str, search: list[str], fuzzy: bool) -> bool:
+
+def search_file(path: str, search: list[str], fuzzy: bool, regex: bool) -> bool:
     """Returns true if the file at path contains every string in search."""
-    #Make sure the script doesn't crash on a read error for a single file.
+    # Make sure the script doesn't crash on a read error for a single file.
     try:
         fi = open(path, "r")
         fi_str = fi.read()
         fi.close()
     except:
-        verbose_print("Could not open " + path)
+        verbose_print("Could not open " + path, 1)
         return False
 
     if not fuzzy:
         for s in search:
-            if not s in fi_str:
-                return False
+            if not regex:
+                if not s in fi_str:
+                    return False
+            else:
+                if re.search(s, fi_str) is None:
+                    return False
         return True
     else:
         for s in search:
-            if s in fi_str:
-                return True
+            if not regex:
+                if s in fi_str:
+                    return True
+            else:
+                if re.search(s, fi_str) is not None:
+                    return True
         return False
 
-def search_path(path: str, search: list[str], fuzzy: bool):
+
+def search_path(path: str, search: list[str], fuzzy: bool, regex: bool) -> None:
     """Searches a directory/file for a given string."""
-    #If it's a directory, run search_path on the contents.
+    # If it's a directory, run search_path on the contents.
     if os.path.isdir(path):
         files = os.listdir(path)
         for fi in files:
-            fipath = path + "/" + fi
-            search_path(fipath, search, fuzzy)
-    #If it's a file, search the file.
+            fipath = os.path.join(path, fi)
+            search_path(fipath, search, fuzzy, regex)
+    # If it's a file, search the file.
     else:
-        if search_file(path, search, fuzzy):
-            print(path)
+        if search_file(path, search, fuzzy, regex):
+            if VERBOSE < 2:
+                click.echo(path)
+            verbose_print(f"match: {path}", 2)
+        else:
+            verbose_print(f"no match: {path}", 2)
 
-def main():
+
+@click.command()
+@click.argument("filename", required=True, type=str)
+@click.option("-v", "--verbose", count=True, type=int, default=0, help="verbose mode. More v = more verbose")
+@click.option("-f", "--fuzzy", is_flag=True, type=bool, default=False, help="fuzzy search, passes check if one string matches instead of all strings")
+@click.option("-r", "--re", "--regex", is_flag=True, default=False, type=bool, help="regex mode, matches regular expressions rather than string literals")
+@click.argument("search_terms", required=True, type=str, nargs=-1)
+def search(*args, **kwargs: [bool, int]) -> None:
     """Program's main function."""
-    if len(argv) < 3 or "-h" in argv or "--help" in argv:
-        print(HELP_MSG)
-        return False
-    #Set verbose
+    # Set verbose
     global VERBOSE
-    if "-v" in argv or "--verbose" in argv:
-        VERBOSE = True
-    else:
-        VERBOSE = False
-    if "-f" in argv or "--fuzzy" in argv:
-        fuzzy = True
-    else:
-        fuzzy = False
-    search_terms = []
-    for a in argv[2:]:
-        if not a in EXCLUDED_SEARCH_TERMS:
-            search_terms.append(a)
-    #Search path.
-    search_path(argv[1], search_terms, fuzzy)
+    VERBOSE = kwargs["verbose"]
+    verbose_print(f"Got verbose level: {VERBOSE}", 1)
+    # Search path.
+    search_path(kwargs["filename"], kwargs["search_terms"],
+                kwargs["fuzzy"], kwargs["re"])
+
 
 if __name__ == "__main__":
-    main()
+    search()
